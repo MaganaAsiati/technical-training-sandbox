670db1ab48b75f2c857cb64ba691e1d3cb65a120
assia <johndoe@example.com>
2024-01-18 14:49:02 +0200

    create new module

---
+++ estate/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    "name": "Real Estate Assia",  # The name that will appear in the App list
+    'description': """
+        This module customise rental module.
+    """,
+    # "version": "16.0.0",  # Version
+    "application": True,  # This line says the module is an App, and not a module
+    "depends": ["sale_renting"],  # dependencies
+    "data": [
+
+    ],
+    "installable": True,
+    'license': 'LGPL-3',
+}
