c33abf19ac350b24e1ea52f2d68b52d43629786c
assia <johndoe@example.com>
2024-01-22 14:55:32 +0200

    Add menu in manifest data

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -9,7 +9,7 @@
     "data": [
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
-        'views/estate_menu_views.xml'
+        'views/estate_menus.xml'
 
     ],
     "installable": True,
