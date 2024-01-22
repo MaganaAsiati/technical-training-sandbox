8a5b0d604845a0607f257939034e8c00163335d4
assia <johndoe@example.com>
2024-01-22 14:56:42 +0200

    Add state and active fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,6 +1,6 @@
 
 from odoo import fields, models
-
+from datetime import datetime, timedelta
 
 class estate_property(models.Model):
     _name = "estate.property"
@@ -9,17 +9,33 @@ class estate_property(models.Model):
     
     name = fields.Char(required=True)
     description = fields.Text()
-    postestate_propertycode = fields.Char()
-    date_availability = fields.Date()
+    postcode = fields.Char()
+    date_availability = fields.Date(string='Availability Date', copy=False, default=lambda self: (datetime.now() + timedelta(days=90)).date())
     expected_price = fields.Float(required=True)
-    selling_price  = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price  = fields.Float(string='Selling Price', readonly=True)
+    bedrooms = fields.Integer(string='Number of Bedrooms', default=2)
     living_area=  fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
-    garden = fields.Boolean()
+    active  = fields.Boolean(default=False)
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
-        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')],)
+        string='garden_orientation',
+        selection=[
+        ('north', 'North'),
+        ('south', 'South'), 
+        ('east', 'East'),
+         ('west', 'West')],)
+    state = fields.Selection(
+            string='state',
+            default='new', 
+            required=True,
+            copy=False,
+            selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled')],)
+
     
